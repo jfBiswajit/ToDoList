b486ae2b7873a714cfb51cd7b199d81d2bff2f1d
Biswajit Biswas <jfbiswajit@gmail.com>
2019-09-06 21:44:50 +0600

    rewrite code

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,6 +1,39 @@
-let taskList = document.querySelector('#taskList');
-let newTask = document.querySelector('#task');
-let formTask = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
 
-taskList.addEventListener('click', deleteTask);
-formTask.addEventListener('submit', addNewTask);
+// Call event listener
+form.addEventListener('submit', addTask);
+taskInput.addEventListener('keyup', filterTask);
+
+// Add new task
+function addTask(e) {
+  if (taskInput.value !== '') {
+    const li = document.createElement('li');
+    li.className = 'task-item delete-task';
+    const task = document.createTextNode(taskInput.value);
+    li.appendChild(task);
+    const link = document.createElement('a');
+    link.setAttribute('href', '#');
+    link.innerHTML = `<i class="fa fa-check"></i>`;
+    li.appendChild(link);
+    ul.appendChild(li);
+    taskInput.value = '';
+  } else {
+    alert('Please add a task');
+  }
+  e.preventDefault();
+}
+// Filter task
+taskInput.addEventListener('keyup', filterTask);
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+  const lis = document.querySelectorAll('.task-item');
+  lis.forEach(task => {
+    if (task.textContent.indexOf(text) === -1) {
+      task.style.display = 'none';
+    } else {
+      task.style.display = 'block';
+    }
+  });
+}
